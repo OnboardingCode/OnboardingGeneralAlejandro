ead7488dcbe21d76ca406641753f2f6e9fb170ef
devAnisan <animan667@gmail.com>
2025-11-06 19:53:29 -0600

    some math

---
+++ arithmeticOp/index.js
@@ -0,0 +1,30 @@
+let students = 30;
+
+//students = students + 1
+//students = students - 1
+//students = students * 1
+//students = students / 1
+//students = students ** 2;
+//let extraStudents = students % 3;
+
+//students +=1;
+//students -=1;
+//students *=1;
+//students /=2;
+//students **=2;
+//students %=2;
+
+//students++;
+//students--;
+
+/*
+    operator precedence
+    1. Parentheses ()
+    2. Exponents **
+    3. Multiplication * , Division / , Remainder %
+    4. Addition + , Subtraction -
+*/
+
+let result = 1 + 2 * 3 + 4 ** 2;
+
+console.log(result);
