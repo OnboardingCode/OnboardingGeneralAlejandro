776ee679c13d01d05fcccfefebb4ef1b63729719
devAnisan <animan667@gmail.com>
2025-11-17 14:35:15 -0600

    ternary operator

---
+++ ternaryOp/index.js
@@ -0,0 +1,24 @@
+/*let age = 17;
+let message = age >= 18 ? "You are an adult" : "You are a minor";
+
+console.log(message);
+*/
+
+/*let time = 16;
+let greeting = time <12 ? "Good morning" : "Good afternoon";
+
+console.log(greeting);
+*/
+
+/*let isStudent = true;
+let message = isStudent ? "You are a student" : "You are not a student";
+
+console.log(message);
+*/
+
+let purchaseAmount = 125;
+let discount = purchaseAmount >= 100 ? 10 : 0;
+
+console.log(
+  `Your total is $${purchaseAmount - purchaseAmount * (discount / 100)}`
+);
